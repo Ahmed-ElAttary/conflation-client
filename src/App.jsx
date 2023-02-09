1485b0d39757f72130d79d44db5c4e1a553f065b
Ahmed Elattary <ahmedelattary@gmail.com>
2023-02-09 16:51:15 +0200

    upload multiple files

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,43 @@ import "./App.css";
 import axios from "axios";
 function App() {
   const [data, setData] = useState([]);
-  useEffect(() => {
-    axios
-      .get("http://localhost:1234/")
-      .then((response) => {
-        setData(response.data);
-      })
-      .catch(function (error) {
-        console.log(error);
-      })
-      .finally(function () {});
-  }, []);
+  const [files, setFiles] = useState([]);
+
+  // useEffect(() => {
+  //   axios
+  //     .get("http://localhost:1234/")
+  //     .then((response) => {
+  //       setData(response.data);
+  //     })
+  //     .catch(function (error) {
+  //       console.log(error);
+  //     })
+  //     .finally(function () {});
+  // }, []);
+  const changeInputFileHandler = (e) => {
+    setFiles(Array.from(e.target.files));
+  };
+  const upload = async () => {
+    const formData = new FormData();
+    files.forEach((el) => {
+      formData.append("file", el);
+    });
+
+    //formData.append("fileName", fileName);
+    try {
+      const res = await axios.post("http://localhost:1234/upload", formData);
+      console.log(res);
+    } catch (ex) {
+      console.log(ex);
+    }
+  };
 
   return (
     <div className="App">
-      {data.length && data.map((el) => <div key={el.id}>{el.name}</div>)}
+      <input type="file" onChange={changeInputFileHandler} multiple></input>
+      {files.length && files.map((el) => <div key={el.name}>{el.name}</div>)}
+      <br />
+      <button onClick={upload}>upload</button>
     </div>
   );
 }
